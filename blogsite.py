f896cb46ae1f517008fc9c0d64eae84aa70f76d0
Trevor Karl <trevorkarl5@gmail.com>
2023-02-23 16:52:53 -0600

    Adding Templates and CSS

--- blogsite.py
+++ blogsite.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Trevor Karl',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 23, 2023'
+    },
+    {
+        'author': 'Trevor Karl',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 24, 2023'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return "<h1>Home Page</h1>"
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 # Turn on debugger with $env:FLASK_DEBUG=1
 # OR use python blogsite.py bc of main method below
