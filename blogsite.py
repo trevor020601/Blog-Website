7bbca0788eaae9842d36994fbbd47b41943575d8
Trevor Karl <trevorkarl5@gmail.com>
2023-02-24 18:12:20 -0600

    Sign Up and Log In Forms

--- blogsite.py
+++ blogsite.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fa827ba53c8130521feb6706f0c0892d'
 
 posts = [
     {
@@ -24,6 +27,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    '''
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    '''
+    return render_template('login.html', title='Login', form=form)
 
 # Turn on debugger with $env:FLASK_DEBUG=1
 # OR use python blogsite.py bc of main method below
